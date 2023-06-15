d33f3055e2ccd9dcbac04b5b13ebd3002d339900
Havenhai <havenhailemariam12@gmail.com>
2023-06-15 13:02:52 -0700

    and that piece of

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print exactly a text to standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
