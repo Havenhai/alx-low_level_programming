1edfcbba98da74673d52c252d33ec587edd0fb3f
Havenhai <havenhailemariam12@gmail.com>
2023-06-15 04:54:24 -0700

    with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - c program that prints that use the function puts
+ *return (0)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
