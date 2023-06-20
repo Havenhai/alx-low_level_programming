0310af583ffdbe97d859e4a796ca58f7cb7e5745
Havenhai <havenhailemariam12@gmail.com>
2023-06-20 04:20:33 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char loweralphabet;
+
+	for (loweralphabet = 'a'; loweralphabet <= 'z'; loweralphabet++)
+	_putchar(loweralphabet);
+	_putchar('\n');
+}
