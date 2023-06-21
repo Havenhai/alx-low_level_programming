73ede0d60dc82f34cbb441bae99eb64a5654068e
Havenhai <havenhailemariam12@gmail.com>
2023-06-20 19:22:53 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even fibonacci unmbers
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int h = 0;
+	long y = 1, k = 2, sum = k;
+
+	while (k + y < 4000000)
+	{
+	k += y;
+	if (k % 2 == 0)
+		sum += k
+			y = k - y;
+	++h;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
