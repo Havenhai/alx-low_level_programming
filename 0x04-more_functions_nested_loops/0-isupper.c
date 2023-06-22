5c05382afd6540156e56a471e00d1756952a984c
Havenhai <havenhailemariam12@gmail.com>
2023-06-22 02:22:20 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase character and tell if the letter is uppercase
+ *
+ * @h: The number to be checked
+ *
+ * Return: 1  if h is uppercase and 0 otherwise
+ */
+
+int _isupper(int h)
+{
+	if (h >= 65 && x <= 90)
+	{
+		return (1);
+	}
+		return (0);
+}
