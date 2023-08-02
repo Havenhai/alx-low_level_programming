2e5aaefaa5994767b32f4facaf10d3ff94907393
Havenhai <havenhailemariam12@gmail.com>
2023-08-01 19:18:12 -0700

    s

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,11 +10,11 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node = NULL;
-	listint_t *next_node = NULL;
+	listint_t *next = NULL;
 
 	while (*head)
 	{
-		next_node = (*head)->next;
+		next = (*head)->next;
 		(*head)->next = prev_node;
 		prev_node = *head;
 		*head = next;
