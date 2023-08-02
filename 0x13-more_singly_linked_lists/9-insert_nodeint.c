3f722d812b31d9bf4b0a53d7f337b15014c1bcfb
Havenhai <havenhailemariam12@gmail.com>
2023-08-01 19:38:12 -0700

    singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,15 +6,15 @@
  * @n: THE data to be inserted in the new node
  * @head: Double pointer to the first node in the list
  * function allowed to modify the head pointer if necessary
- * @i: Index where the new node is added
+ * @idx: Index where the new node is added
  * Return: New node or NULL
  */
 
-listint_t *insert_nodeint_at_ind(listint_t **head, unsigned int i, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int hyt;
 	listint_t *new_node;
-	listint_t *temppry = *head;
+	listint_t *temppy = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node || !head)
@@ -23,23 +23,23 @@ listint_t *insert_nodeint_at_ind(listint_t **head, unsigned int i, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (i == 0)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	for (hyt = 0; temppry && hyt < i; hyt++)
+	for (hyt = 0; temppy && hyt < idx; hyt++)
 	{
-		if (hyt == i - 1)
+		if (hyt == idx - 1)
 		{
-			new_node->next = temppry->next;
-			temppry->next = new_node;
+			new_node->next = temppy->next;
+			temppy->next = new_node;
 			return (new_node);
 		}
 		else
-			temppry = temppry->next;
+			temppy = temppy->next;
 	}
 
 	return (NULL);
