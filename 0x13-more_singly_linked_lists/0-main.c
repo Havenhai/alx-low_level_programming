d83b275adf8253359f3b0611dca691677ca30b18
Havenhai <havenhailemariam12@gmail.com>
2023-08-01 17:21:13 -0700

    l

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -4,29 +4,29 @@
 #include "lists.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t ntimes;
+listint_t *head;
+listint_t *new;
+listint_t hello = {8, NULL};
+size_t ntimes;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->ntimes = 9;
-    new->next_node = head;
-    head = new;
-    ntimes = print_listint(head);
-    printf("-> %lu elements\n", ntimes);
-    free(new);
-    return (0);
+head = &hello;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+printf("Error\n");
+return (1);
+}
+new->ntimes = 9;
+new->next_node = head;
+head = new;
+ntimes = print_listint(head);
+printf("-> %lu elements\n", ntimes);
+free(new);
+return (0);
 }
