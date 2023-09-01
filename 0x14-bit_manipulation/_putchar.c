01abe96d973d567bc519999fd9276e9259fa643d
Havenhai <havenhailemariam12@gmail.com>
2023-08-31 19:02:44 -0700

    up

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,24 +1,13 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - Write a character to the standard output.
- * @character: The character to be written.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * This function takes a single character
- * 'character' as a parameter and
- * utilizes the 'write' system call to write
- * the character to the standard output.
- * The function returns the number of bytes
- * written on success, and in case of an
- * error, it returns -1 and sets 'errno'
- * to indicate the specific error that occurred.
- *
- * @character: The character to write the standard output.
- * Return: The number of bytes written on success,
- * or -1 on error.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char character)
+int _putchar(char c)
 {
-	return (write(1, &character, 1));
+	return (write(1, &c, 1));
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,7 +8,7 @@ int clear_bit(unsigned long int *num_havu_base, unsigned int bit_havu_idx);
 void Print_binary(unsigned long int n_deci);
 unsigned int flip_bits(unsigned long int num_ha, unsigned long int num_hb);
 int _atoi(const char *s);
-int _putchar(char character);
+int _putchar(char c);
 unsigned int binary_strg_to_uint(const char *bina);
 
 #endif
