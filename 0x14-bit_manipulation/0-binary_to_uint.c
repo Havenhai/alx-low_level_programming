ccd5fa8ecc072e3123bad993d4bc60dbe57e68bb
Havenhai <havenhailemariam12@gmail.com>
2023-08-31 19:11:35 -0700

    up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,25 @@
 #include "main.h"
 
 /**
- * binary_strg_to_uint - Converts a binary string
- * to an unsigned integer.
- * This function takes a null-terminated binary string
- * as input and converts
- * it into an equivalent unsigned integer value.
- * The binary string is assumed
- * to contain only '0' and '1' characters
- * representing the binary digits.
- * function iterates through each character in the string
- * If the input string is not a valid binary
- * representation, the function returns 0.
- * If the input string is NULL, the
- * function also returns 0.
- * @bina: The input string containing the binary number.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted unsigned integer
- * or 0 if input is invalid or NULL.
+ * Return: the converted number
  */
-unsigned int binary_strg_to_uint(const char *bina)
+unsigned int binary_to_uint(const char *b)
 {
-	int binary_i;
-	unsigned int decimal_output_result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (!bina)
+	if (!b)
 		return (0);
 
-	for (binary_i = 0; bina[binary_i]; binary_i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (bina[binary_i] < '0' || bina[binary_i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-	decimal_output_result = 2 * decimal_output_result + (bina[binary_i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (decimal_output_result);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int Get_bit(unsigned long int num_sys_base, unsigned int index_bit);
-int set_bit(unsigned long int *numeral_sys, unsigned int index_havubit);
-int get_endianness(void);
-int clear_bit(unsigned long int *num_havu_base, unsigned int bit_havu_idx);
-void Print_binary(unsigned long int n_deci);
-unsigned int flip_bits(unsigned long int num_ha, unsigned long int num_hb);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _atoi(const char *s);
 int _putchar(char c);
-unsigned int binary_strg_to_uint(const char *bina);
+int get_endianness(void);
 
 #endif
