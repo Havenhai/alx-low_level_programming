6796c4c25adbebf50b6775dd64832031bb86a80c
Havenhai <havenhailemariam12@gmail.com>
2023-08-31 19:19:47 -0700

    up

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+unsigned int binary_to_uint(const char *b);
 int Get_bit(unsigned long int num_sys_base, unsigned int index_bit);
 int set_bit(unsigned long int *numeral_sys, unsigned int index_havubit);
 int get_endianness(void);
@@ -9,6 +10,5 @@ void Print_binary(unsigned long int n_deci);
 unsigned int flip_bits(unsigned long int num_ha, unsigned long int num_hb);
 int _atoi(const char *s);
 int _putchar(char c);
-unsigned int binary_strg_to_uint(const char *bina);
 
 #endif
