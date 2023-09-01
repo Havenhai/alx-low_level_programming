6ab5cc764883a6843a54c3dd1f2789b207be6b4f
Havenhai <havenhailemariam12@gmail.com>
2023-08-31 19:25:21 -0700

    hu

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,38 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * binary_to_uint - Converts a binary string
+ * to an unsigned integer.
+ * This function takes a null-terminated binary string
+ * as input and converts
+ * it into an equivalent unsigned integer value.
+ * The binary string is assumed
+ * to contain only '0' and '1' characters
+ * representing the binary digits.
+ * function iterates through each character in the string
+ * If the input string is not a valid binary
+ * representation, the function returns 0.
+ * If the input string is NULL, the
+ * function also returns 0.
+ * @b: The input string containing the binary number.
  *
- * Return: the converted number
+ * Return: The converted unsigned integer
+ * or 0 if input is invalid or NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int binary_i;
+	unsigned int decimal_output_result = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (binary_i = 0; b[binary_i]; binary_i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[binary_i] < '0' || b[binary_i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+	decimal_output_result = 2 * decimal_output_result + (b[binary_i] - '0');
 	}
 
-	return (dec_val);
+	return (decimal_output_result);
 }
