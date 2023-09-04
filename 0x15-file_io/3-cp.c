90501a0a6c84bbc7e9163920bc684fc04b228c2b
Havenhai <havenhailemariam12@gmail.com>
2023-09-04 16:48:38 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,102 +2,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
-
 /**
- * create_buffer - havu function allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * allocate_buffer - Allocates a 1024-byte buffer for
+ * copying characters between files.
  *
+ * This function dynamically allocates
+ * memory for a buffer that will be used to read
+ * and write characters when copying from one file to another.
+ *
+ * @destination_file: The name of the destination file
+ * where characters are stored.
  * Return: A pointer to the newly-allocated buffer.
+ * If memory allocation fails, the function prints
+ * an error message to the standard error
+ * stream (STDERR_FILENO) and exits with a status code of 99.
  */
-char *create_buffer(char *file)
+char *allocate_buffer(char *destination_file)
 {
-	char *buffer;
+char *copy_buffer;
 
-	buffer = malloc(sizeof(char) * 1024);
+copy_buffer = malloc(sizeof(char) * 1024);
 
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
+if (copy_buffer == NULL)
+{
+dprintf(STDERR_FILENO,
+"Error: Unable to allocate memory for writing to %s\n", destination_file);
+exit(99);
+}
 
-	return (buffer);
+return (copy_buffer);
 }
 
 /**
- * close_file - havu function closes file descriptors.
+ * close_file_descriptor - Closes a file descriptor.
+ *
+ * This function closes a given file descriptor
+ * and checks for any errors during the
+ * closing process.
+ *
  * @fd: The file descriptor to be closed.
+ * Return: 0 on success
+ * If the file descriptor cannot be closed successfully,
+ * the function prints an error
+ * message to the standard error stream (STDERR_FILENO)
+ * and exits with a status code of 100.
  */
-void close_file(int fd)
+void close_file_descriptor(int fd)
 {
-	int c;
+int close_result;
 
-	c = close(fd);
+close_result = close(fd);
 
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+if (close_result == -1)
+{
+dprintf(STDERR_FILENO, "Error: Unable to close file descriptor %d\n", fd);
+exit(100);
+}
 }
 
 /**
- * main - havu function copies the contents of a file to another file.
+ * main - Copies the contents of one file to another file.
+ *
+ * This is the main function of the program,
+ * responsible for copying the contents of one
+ * source file to another destination file.
+ * It handles command line arguments, file I/O,
+ * and error handling.
+ *
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argv: An array of pointers to the program's arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
+ * If the argument count is incorrect,
+ * the function prints a usage message to the standard
+ * error stream and exits with a status code of 97.
+ * If the source file does not exist or cannot be read,
+ * it prints an error message and exits
+ * with a status code of 98.
+ * If the destination file cannot be created or written to,
+ * it prints an error message and exits
+ * with a status code of 99.
+ * If there are errors during file operations,
+ * it prints appropriate error messages and exits
+ * with a status code of 100.
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+int source_fd, destination_fd, read_result, write_result;
+char *copy_buffer;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp source_file destination_file\n");
+exit(97);
+}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+copy_buffer = allocate_buffer(argv[2]);
+source_fd = open(argv[1], O_RDONLY);
+read_result = read(source_fd, copy_buffer, 1024);
+destination_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
+do {
+if (source_fd == -1 || read_result == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
+free(copy_buffer);
+exit(98);
+}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
+write_result = write(destination_fd, copy_buffer, read_result);
+if (destination_fd == -1 || write_result == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
+free(copy_buffer);
+exit(99);
+}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+read_result = read(source_fd, copy_buffer, 1024);
+destination_fd = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
-
-	return (0);
+} while (read_result > 0);
+free(copy_buffer);
+close_file_descriptor(source_fd);
+close_file_descriptor(destination_fd);
+return (0);
 }
