1678c923f3aad3ed7e0278f9d5f42be925225ed0
Havenhai <havenhailemariam12@gmail.com>
2023-09-04 15:53:53 -0700

    fle

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,33 +2,39 @@
 
 /**
  * append_text_to_file - havu function appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
+ * This function appends the provided text to the end of the specified file.
  *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ * @filename: A pointer to the name of the file to
+ * which the text will be appended.
+ * @text_content: A pointer to the string of text that will be
+ * added to the end of the file.
+ *
+ * Return: If the function fails due to an invalid filename
+ * or if filename is NULL, it returns -1.
+ * If file does not exist or the user lacks write permissions, returns -1.
+ * If the text_content is NULL or the write operation fails, it returns -1.
+ * If the text is successfully appended to the file, it returns 1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file_descriptor, wwrite_result, len_txt = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len_txt = 0; text_content[len_txt];)
+			len_txt++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	wwrite_result = write(file_descriptor, text_content, len_txt);
 
-	if (o == -1 || w == -1)
+	if (file_descriptor == -1 || wwrite_result == -1)
 		return (-1);
 
-	close(o);
+	close(file_descriptor);
 
 	return (1);
 }
